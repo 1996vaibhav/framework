a8fac9a302a623d3f50a570b29f2e28f1243a81a
vaibhavsonar <sonarvaibhav22@gmail.com>
2026-06-07 01:08:41 +0530

    check jenkins cron job two

--- src/test/java/com/qa/callhippo/tests/AccountPageTest.java
+++ src/test/java/com/qa/callhippo/tests/AccountPageTest.java
@@ -24,7 +24,6 @@ public class AccountPageTest extends BaseTest {
 	@Test
 	public void isLogoutLinkExistTest() {
 		Assert.assertTrue(accountPage.isLogoutLinkExist());
-		System.out.println("Test");
 		
 	}
 	
